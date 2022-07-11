99b473a36271295356b175b9217f9d4e8a4d2af3
kizito chidike <chidyke.k@gmail.com>
2022-07-11 09:41:55 +0100

    modify __init__.py & models.py

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -3,30 +3,75 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
+import time
 
 from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_categories(request, selection):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+
+    categories = [category.format() for category in selection]
+    current_categories = categories[start:end]
+
+    return current_categories
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
-
+    CORS(app)
     """
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
-
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
     """
     @TODO: Use the after_request decorator to set Access-Control-Allow
     """
-
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
     """
     @TODO:
     Create an endpoint to handle GET requests
     for all available categories.
     """
+    @app.route('/categories', methods=['GET'])
+    def retrieve_categories():
+        selection = Category.query.order_by(Category.id).all()
+        current_categories = paginate_categories(request, selection)
+        
+        if len(current_categories) == 0:
+            abort(404)
 
+        final_categories = {}
+        for x in current_categories:
+            final_categories.update({x['id']: x['type']})
+           
+        return jsonify({
+            'success':True,
+            'categories': final_categories,
+            #'total_categories': len(Category.query.all())
+        })
+
+       #return 'Category %d' % category_id
+    
+    {
+      "categories": {
+          "1": "Science", 
+          "2": "Art", 
+          "3": "Geography", 
+          "4": "History", 
+          "5": "Entertainment", 
+          "6": "Sports"
+      }, 
+      "success": True
+  }
 
     """
     @TODO:
@@ -40,7 +85,113 @@ def create_app(test_config=None):
     ten questions per page and pagination at the bottom of the screen for three pages.
     Clicking on the page numbers should update the questions.
     """
+    @app.route('/questions')
+    def get_questions():
 
+        page = request.args.get('page', 1, type=int)
+        start = (page - 1) * 10
+        end =start + 10
+
+        questions = Question.query.all()
+        formatted_questions = [question.format() for question in questions]
+
+        selection = Category.query.order_by(Category.id).all()
+        current_categories = paginate_categories(request, selection)
+        
+        if len(current_categories) == 0:
+            abort(404)
+
+        final_categories = {}
+        for x in current_categories:
+            final_categories.update({x['id']: x['type']})
+
+        return jsonify({
+            'success': True,
+            'questions': formatted_questions[start:end],
+            'total_questions':len(formatted_questions),
+            'categories': final_categories,
+            'current_category': 1
+
+            })
+    return app
+    {
+      "categories": {
+          "1": "Science", 
+          "2": "Art", 
+          "3": "Geography", 
+          "4": "History", 
+          "5": "Entertainment", 
+          "6": "Sports"
+      }, 
+      "questions": [ 
+          {
+              "answer": "Muhammad Ali", 
+              "category": 6, 
+              "difficulty": 1, 
+              "id": 9, 
+              "question": "What boxer's original name is Cassius Clay?"
+          }, 
+          {
+              "answer": "Apollo 13", 
+              "category": 5, 
+              "difficulty": 4, 
+              "id": 2, 
+              "question": "What movie earned Tom Hanks his third straight Oscar nomination, in 1996?"
+          }, 
+          {
+              "answer": "Tom Cruise", 
+              "category": 5, 
+              "difficulty": 4, 
+              "id": 4, 
+              "question": "What actor did author Anne Rice first denounce, then praise in the role of her beloved Lestat?"
+          }, 
+          {
+              "answer": "Edward Scissorhands", 
+              "category": 5, 
+              "difficulty": 3, 
+              "id": 6, 
+              "question": "What was the title of the 1990 fantasy directed by Tim Burton about a young man with multi-bladed appendages?"
+          }, 
+          {
+              "answer": "Brazil", 
+              "category": 6, 
+              "difficulty": 3, 
+              "id": 10, 
+              "question": "Which is the only team to play in every soccer World Cup tournament?"
+          }, 
+          {
+              "answer": "Uruguay", 
+              "category": 6, 
+              "difficulty": 4, 
+              "id": 11, 
+              "question": "Which country won the first ever soccer World Cup in 1930?"
+          }, 
+          {
+              "answer": "George Washington Carver", 
+              "category": 4, 
+              "difficulty": 2, 
+              "id": 12, 
+              "question": "Who invented Peanut Butter?"
+          }, 
+          {
+              "answer": "Lake Victoria", 
+              "category": 3, 
+              "difficulty": 2, 
+              "id": 13, 
+              "question": "What is the largest lake in Africa?"
+          }, 
+          {
+              "answer": "The Palace of Versailles", 
+              "category": 3, 
+              "difficulty": 3, 
+              "id": 14, 
+              "question": "In which royal palace would you find the Hall of Mirrors?"
+          }
+      ], 
+      "success": True, 
+      "total_questions": 19
+  }
+  
     """
     @TODO:
     Create an endpoint to DELETE question using a question ID.
@@ -48,8 +199,36 @@ def create_app(test_config=None):
     TEST: When you click the trash icon next to a question, the question will be removed.
     This removal will persist in the database and when you refresh the page.
     """
+    
+    @app.route('/questions/<int:id>', methods=['DELETE'])
+    def delete_question(id):
+        #print('got here')
+        try:
+            #print(question_id)
+            question = Question.query.get(id)
 
+            #print(question)
+                
+            if question is None:
+                abort(404)
+
+            question.delete()
+            selection = Question.query.order_by(id).all()
+            current_questions = paginate_questions(request, selection)
+
+            return jsonify ({
+              'success': True,
+               
+            })
+
+        except:
+            abort(422)
+        
+    
     """
+    '''deleted': question_id, 
+              'questions': current_questions,
+              'total_questions': len(Question.query.all())'''
     @TODO:
     Create an endpoint to POST a new question,
     which will require the question and answer text,
@@ -58,8 +237,105 @@ def create_app(test_config=None):
     TEST: When you submit a question on the "Add" tab,
     the form will clear and the question will appear at the end of the last page
     of the questions list in the "List" tab.
-    """
+    """    
+    @app.route('/questions', methods=['POST'])
+    def create_question():
+        body = request.get_json()
 
+        new_question = body.get('question', None)
+        new_answer = body.get('answer', None)
+        new_difficulty = body.get('difficulty', None)
+        new_category = body.get('category', None)
+        
+        try:
+            question = Question(question=new_question, answer=new_answer, difficulty=new_difficulty, category=new_category)
+            question.insert()
+
+            return jsonify ({
+              'success': True,
+            })
+        
+        except:
+            abort(422)
+        
+    {
+      "created": 173, 
+      "question_created": "Which US state contains an area known as the Upper Penninsula?", 
+      "questions": [
+          {
+              "answer": "Apollo 13", 
+              "category": 5, 
+              "difficulty": 4, 
+              "id": 2, 
+              "question": "What movie earned Tom Hanks his third straight Oscar nomination, in 1996?"
+          }, 
+          {
+              "answer": "Tom Cruise", 
+              "category": 5, 
+              "difficulty": 4, 
+              "id": 4, 
+              "question": "What actor did author Anne Rice first denounce, then praise in the role of her beloved Lestat?"
+          }, 
+          {
+              "answer": "Muhammad Ali", 
+              "category": 4, 
+              "difficulty": 1, 
+              "id": 9, 
+              "question": "What boxer's original name is Cassius Clay?"
+          }, 
+          {
+              "answer": "Brazil", 
+              "category": 6, 
+              "difficulty": 3, 
+              "id": 10, 
+              "question": "Which is the only team to play in every soccer World Cup tournament?"
+          }, 
+          {
+              "answer": "Uruguay", 
+              "category": 6, 
+              "difficulty": 4, 
+              "id": 11, 
+              "question": "Which country won the first ever soccer World Cup in 1930?"
+          }, 
+          {
+              "answer": "George Washington Carver", 
+              "category": 4, 
+              "difficulty": 2, 
+              "id": 12, 
+              "question": "Who invented Peanut Butter?"
+          }, 
+          {
+              "answer": "Lake Victoria", 
+              "category": 3, 
+              "difficulty": 2, 
+              "id": 13, 
+              "question": "What is the largest lake in Africa?"
+          }, 
+          {
+              "answer": "The Palace of Versailles", 
+              "category": 3, 
+              "difficulty": 3, 
+              "id": 14, 
+              "question": "In which royal palace would you find the Hall of Mirrors?"
+          }, 
+          {
+              "answer": "Agra", 
+              "category": 3, 
+              "difficulty": 2, 
+              "id": 15, 
+              "question": "The Taj Mahal is located in which Indian city?"
+          }, 
+          {
+              "answer": "Escher", 
+              "category": 2, 
+              "difficulty": 1, 
+              "id": 16, 
+              "question": "Which Dutch graphic artist\u2013initials M C was a creator of optical illusions?"
+          }
+      ], 
+      "success": True, 
+      "total_questions": 20
+  }
     """
     @TODO:
     Create a POST endpoint to get questions based on a search term.
@@ -70,6 +346,122 @@ def create_app(test_config=None):
     only question that include that string within their question.
     Try using the word "title" to start.
     """
+    @app.route('/questions/search', methods=['POST'])
+    def create_questions():
+        body = request.get_json()
+
+        new_titles = question.get('titles', None)
+        new_totalQuestions = questions.get('totalQuestions', None)
+        new_currentCategory = question.get('currentCategory', None)
+        search =  question.get('search', None)
+
+        try:
+          if search:
+            selection =  Question.query.order_by(id).filter(Question.title.ilke('%{}%'.format(search)))
+            current_questions = paginate_questions(request, selection)
+
+            return jsonify({
+                'success': True,
+                'questions': current_questions
+                'total_questions': len(selection.all())
+            })
+
+        except:
+            abort(422)
+        '''else:
+          question = Question(title=new_title, totalQuestions=new_totalQuestions, curentCategory=new_currentCategory)
+          book.insert()
+
+          selection = Question.query.order_by(id).all()
+          current_questions = paginate_books(request, selection)
+
+          return jsonify({
+              'success': True,
+              'created': book.id,
+              'question': current_questions, 
+              'total_quetions': len(Question.query.all())
+          })
+      '''
+        
+        
+    {
+      "created": 173, 
+      "question_created": "Which US state contains an area known as the Upper Penninsula?", 
+      "questions": [
+          {
+              "answer": "Apollo 13", 
+              "category": 5, 
+              "difficulty": 4, 
+              "id": 2, 
+              "question": "What movie earned Tom Hanks his third straight Oscar nomination, in 1996?"
+          }, 
+          {
+              "answer": "Tom Cruise", 
+              "category": 5, 
+              "difficulty": 4, 
+              "id": 4, 
+              "question": "What actor did author Anne Rice first denounce, then praise in the role of her beloved Lestat?"
+          }, 
+          {
+              "answer": "Muhammad Ali", 
+              "category": 4, 
+              "difficulty": 1, 
+              "id": 9, 
+              "question": "What boxer's original name is Cassius Clay?"
+          }, 
+          {
+              "answer": "Brazil", 
+              "category": 6, 
+              "difficulty": 3, 
+              "id": 10, 
+              "question": "Which is the only team to play in every soccer World Cup tournament?"
+          }, 
+          {
+              "answer": "Uruguay", 
+              "category": 6, 
+              "difficulty": 4, 
+              "id": 11, 
+              "question": "Which country won the first ever soccer World Cup in 1930?"
+          }, 
+          {
+              "answer": "George Washington Carver", 
+              "category": 4, 
+              "difficulty": 2, 
+              "id": 12, 
+              "question": "Who invented Peanut Butter?"
+          }, 
+          {
+              "answer": "Lake Victoria", 
+              "category": 3, 
+              "difficulty": 2, 
+              "id": 13, 
+              "question": "What is the largest lake in Africa?"
+          }, 
+          {
+              "answer": "The Palace of Versailles", 
+              "category": 3, 
+              "difficulty": 3, 
+              "id": 14, 
+              "question": "In which royal palace would you find the Hall of Mirrors?"
+          }, 
+          {
+              "answer": "Agra", 
+              "category": 3, 
+              "difficulty": 2, 
+              "id": 15, 
+              "question": "The Taj Mahal is located in which Indian city?"
+          }, 
+          {
+              "answer": "Escher", 
+              "category": 2, 
+              "difficulty": 1, 
+              "id": 16, 
+              "question": "Which Dutch graphic artist\u2013initials M C was a creator of optical illusions?"
+          }
+      ], 
+      "success": True, 
+      "total_questions": 20
+    }
 
     """
     @TODO:
@@ -79,7 +471,34 @@ def create_app(test_config=None):
     categories in the left column will cause only questions of that
     category to be shown.
     """
-
+    {
+      "current_category": "Science", 
+      "questions": [
+          {
+              "answer": "The Liver", 
+              "category": 1, 
+              "difficulty": 4, 
+              "id": 20, 
+              "question": "What is the heaviest organ in the human body?"
+          }, 
+          {
+              "answer": "Alexander Fleming", 
+              "category": 1, 
+              "difficulty": 3, 
+              "id": 21, 
+              "question": "Who discovered penicillin?"
+          }, 
+          {
+              "answer": "Blood", 
+              "category": 1, 
+              "difficulty": 4, 
+              "id": 22, 
+              "question": "Hematology is a branch of medicine involving the study of what?"
+          }
+      ], 
+      "success": True, 
+      "total_questions": 18
+  }
     """
     @TODO:
     Create a POST endpoint to get questions to play the quiz.
@@ -91,12 +510,25 @@ def create_app(test_config=None):
     one question at a time is displayed, the user is allowed to answer
     and shown whether they were correct or not.
     """
-
+    {
+      "question": {
+          "answer": "Alexander Fleming", 
+          "category": 1, 
+          "difficulty": 3, 
+          "id": 21, 
+          "question": "Who discovered pencilin?"
+      }, 
+      "success": True
+  }
     """
     @TODO:
     Create error handlers for all expected errors
     including 404 and 422.
     """
+    {
+    "success": False,
+    "error": 404,
+    "message": "resource not found"
+}
 
     return app
-
--- backend/models.py
+++ backend/models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = 'trivia'
-database_path = 'postgres://{}/{}'.format('localhost:5432', database_name)
+database_path = 'postgres://{}/{}'.format('localhost:5432', 'trivia')
 
 db = SQLAlchemy()
 
@@ -12,10 +12,11 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 """
-def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+def setup_db(app, database_path="postgresql://postgres:coralshark@localhost:5432/trivia"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:coralshark@localhost:5432/trivia"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
+
     db.init_app(app)
     db.create_all()
 
